eb517979d7f192e30d82479815cb6bb17b260a1a
margallostephen <stephenmargallo04@gmail.com>
2025-10-02 17:56:55 +0800

    Added company logo

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,9 @@
 <footer class="footer">
     <div class="footer-inner">
         <div class="footer-content">
+            <span>
+                <img src="<?php getImagePath('prima-icon.png') ?>" alt="Logo" height="25" width="25" class="img-responsive center-block">
+            </span>
             <span class="bigger-120">
                 <?php echo SYSTEM_NAME; ?> &copy; <?php echo date('Y'); ?>
             </span>
